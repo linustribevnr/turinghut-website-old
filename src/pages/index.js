b20dffe364e25045388894c9529e4c6a22a65e54
khssupriya <khssupriya@gmail.com>
2022-01-22 18:44:15 +0530

    feat: navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
+import NavBar from "../components/NavBar"
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">        
-      Hello world!      
-    </h1>
+    <Router>
+      <NavBar/>
+    </Router>
   )
 }
