a73db93b40e0a0f4e4e5ce3579e66246666c9e0f
khssupriya <khssupriya@gmail.com>
2022-01-25 20:04:57 +0530

    feat: vision mission

---
+++ src/pages/about.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div className="mx-10 min-h-90v">
+      <div className="m-6 w-full flex flex-wrap items-center justify-evenly">
+        <div className="m-6 p-6 max-w-md text-center rounded-lg border shadow-3xl shadow-black bg-gray-800 border-gray-700">
+          <h5 className="mb-2 text-2xl font-bold text-white">Vision</h5>
+
+          <p className="mb-3 font-normal text-gray-400">
+            Act as platform of “unity” to promote the `cause` of Computer
+            Science. Develop a sense of belongingness amongst the pupils in the
+            `impact` being created by Computer Science and it's applied concepts
+            while appreciating its inter-disciplinary nature and recognizing.
+          </p>
+        </div>
+        <div className="m-6 p-6 max-w-md text-center rounded-lg border shadow-3xl shadow-black bg-gray-800 border-gray-700">
+          <h5 className="mb-2 text-2xl font-bold text-white">Mission</h5>
+
+          <p className="mb-3 font-normal text-gray-400">
+            Act as platform of “unity” to promote the `cause` of Computer
+            Science. Develop a sense of belongingness amongst the pupils in the
+            `impact` being created by Computer Science and it's applied concepts
+            while appreciating its inter-disciplinary nature and recognizing.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
