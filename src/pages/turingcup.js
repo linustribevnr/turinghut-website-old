73bebce1fadc4ee1664e86dc7de7b7e980caa134
madhavibatthala <madhavibatthala1103@gmail.com>
2022-05-24 08:17:23 +0530

    added-turingcup-roadmap

--- src/pages/turingcup.js
+++ src/pages/turingcup.js
@@ -1,14 +1,10 @@
 import React from "react"
+import RoadMap from "../components/roadmap"
 
 export default function App() {
   return (
-    <div className="iframe-container">
-      <iframe
-        className="iframe"
-        title="Turing Cup"
-        src="/turing_cup.pdf"
-        frameborder="0"
-      ></iframe>
+    <div>
+      <RoadMap/>
     </div>
   )
 }
