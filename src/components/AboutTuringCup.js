882f4898220cccf21e8023729c7a85045550a3fe
TapanGanesh <tapanganesh.naram@gmail.com>
2022-05-27 20:38:28 +0530

    Turing cup landing page added

---
+++ src/components/AboutTuringCup.js
@@ -0,0 +1,38 @@
+
+import React from "react"
+
+export default function AboutTuringCup() {
+  return (
+    <div className=" pt-12 bg-secondary h-100v">
+      <div className="text-white">
+        <div className="text-4xl md:text-5xl p-3 text-center text-white">
+          <h1 className="p-2">Turing Cup</h1>
+          <h1 className="p-2">One Team, One Dream</h1>
+        </div>
+        <div className="flex-about-reverse items-center justify-center bg-secondary">
+          <div className="text-xl sm:text-xl max-w-2xl">
+            <h1 className="text-2xl md:text-3xl">What is Turing Cup?</h1>
+            <p className="mx-6 pt-3">
+              Turing cup is the National level coding contest of Turing Hut in
+              which teams compete with each other beyond borders and grow more
+              skills as a programmer.
+            </p>
+            <h1 className="text-2xl md:text-3xl pt-5">Eligibility :</h1>
+            <p className="mx-5 pt-3">
+              Students from all years, pursuing a full-time degree in any
+              branch.
+            </p>
+          </div>
+
+          <div className="w-72 h-72 mx-5 flex justify-center ">
+            <img
+              src="./TuringCup.png"
+              alt="Turing Cup Image"
+              className="shadow-lg  bg-body rounded "
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/turingcup.js
+++ src/pages/turingcup.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Roadmap from "../components/roadmap"
 import NavBar from "../components/NavBar"
+import AboutTuringCup from "../components/AboutTuringCup"
 export default function App() {
   return (
     <div>
       <NavBar/>
+      <AboutTuringCup/>
       <Roadmap/>
     </div>
   )
