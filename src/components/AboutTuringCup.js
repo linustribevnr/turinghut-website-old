fccfd194015390ecddb3b23c1b96ca294be8654f
TapanGanesh <tapanganesh.naram@gmail.com>
2022-05-28 18:47:38 +0530

    fix: Image loading and removed empty spaces

--- src/components/AboutTuringCup.js
+++ src/components/AboutTuringCup.js
@@ -1,15 +1,14 @@
-
 import React from "react"
 
 export default function AboutTuringCup() {
   return (
-    <div className=" pt-12 bg-secondary h-100v">
+    <div className="pt-14 bg-secondary h-100v">
       <div className="text-white">
         <div className="text-4xl md:text-5xl p-3 text-center text-white">
           <h1 className="p-2">Turing Cup</h1>
-          <h1 className="p-2">One Team, One Dream</h1>
+          <h1 className="px-2 py-4">One Team, One Dream</h1>
         </div>
-        <div className="flex-about-reverse items-center justify-center bg-secondary">
+        <div className="flex-about-reverse items-center justify-center bg-secondary pt-6">
           <div className="text-xl sm:text-xl max-w-2xl">
             <h1 className="text-2xl md:text-3xl">What is Turing Cup?</h1>
             <p className="mx-6 pt-3">
@@ -26,9 +25,9 @@ export default function AboutTuringCup() {
 
           <div className="w-72 h-72 mx-5 flex justify-center ">
             <img
-              src="./TuringCup.png"
-              alt="Turing Cup Image"
               className="shadow-lg  bg-body rounded "
+              src={`../turingcup/turingcup.jpg`}
+              alt="Turing Cup"
             />
           </div>
         </div>
